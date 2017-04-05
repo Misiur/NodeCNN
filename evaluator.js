08db1d21b192e8d9bacc1f66c152b48860562d76
Zombo <foo@bar.com>
2017-04-05 21:33:15 +0100

    Evaluator as a class

--- evaluator.js
+++ evaluator.js
@@ -7,68 +7,76 @@ const consts = {
   READY: 'READY'
 }
 
-async function createAnalyzer () {
-  const cp = await createCNNProcess();
-  return analyzeTextAddCP(cp);
-}
-
-async function createCNNProcess() {
-  return new Promise((resolve, reject) => {
-    log('Spawning python process');
-    const cp = spawn('python', ['eval.py', 'someOptions'], {
-      cwd: __dirname + '/cnn-text-classification-tf'
+class Evaluator {
+  constructor() {
+    this.STATUS = Object.freeze({
+      NOT_READY: 0,
+      READY: 1,
+      STOPPED: 2,
     });
-
-    cp.stdin.setEncoding('utf-8');
-
-    cp.on('exit', () => {
-      log('Python process died');
-    });
-
-    cp.promiseSolvers = [];
-
-    // register for incoming data
-    cp.stdout.on('data', chunk => {
-      let str = chunk.toString('utf-8');
-
-      str.split(EOL).map(str => {
-        if(str.indexOf(consts.READY) === 0) {
-          log('READY received');
-          resolve(cp);
-          return false;
-        } else if(str.indexOf(consts.ANALYZE) === 0) {
-          str = str.split(consts.ANALYZE)[1].split('\n')[0];
-          const data = JSON.parse(str);
-          cp.promiseSolvers[data.id](data);
-          return false;
-        } else {
-          log(str);
-        }
-      });
-    });
-
-    cp.stderr.on('data', chunk => log(chunk.toString('utf-8')));
-  });
-}
-
-/**
- * async function to analyze text
- * @param text - text to analyze
- * @return - promise
-*/
-const analyzeTextAddCP = function (cp) {
-  let i = 0;
-
-  return async function analyzeText (text) {
-    const promise = new Promise((resolve, reject) => {
-      cp.promiseSolvers.push(resolve);
-    });
-
-    cp.stdin.write(JSON.stringify({ id: i, job: consts.ANALYZE, text }) + '\n');
-
-    i++;
-    return promise;
+    this.status = this.STATUS.NOT_READY;
+    this.cp = null;
+    this.messageId = 0;
   }
-};
 
-module.exports = createAnalyzer;
+  async start() {
+    this.cp = await this.createCNNProcess();
+  }
+
+  async createCNNProcess() {
+    return new Promise((resolve, reject) => {
+      log('Spawning python process');
+      const cp = spawn('python', ['eval.py', 'someOptions'], {
+        cwd: __dirname + '/cnn-text-classification-tf'
+      });
+
+      cp.stdin.setEncoding('utf-8');
+
+      cp.on('exit', () => {
+        log('Python process died');
+      });
+
+      cp.promiseSolvers = [];
+
+      // register for incoming data
+      cp.stdout.on('data', chunk => {
+        let str = chunk.toString('utf-8');
+
+        str.split(EOL).map(str => {
+          if(str.indexOf(consts.READY) === 0) {
+            log('READY received');
+            this.status = this.STATUS.READY;
+            resolve(cp);
+            return false;
+          } else if(str.indexOf(consts.ANALYZE) === 0) {
+            str = str.split(consts.ANALYZE)[1].split(EOL)[0];
+            const data = JSON.parse(str);
+            return false;
+          } else {
+            log(str);
+          }
+        });
+      });
+
+      cp.stderr.on('data', chunk => log(chunk.toString('utf-8')));
+    });
+  }
+
+  stop() {
+    this.cp.kill('SIGINT');
+    this.status = this.STATUS.STOPPED;
+    log('Analyzer stopped');
+  }
+
+  async analyze(text) {
+    if(this.status !== this.STATUS.READY) {
+      throw new Error(`Tried to call analyze when state is not ready (${this.status})`);
+    }
+
+    this.cp.stdin.write(JSON.stringify({ id: this.messageId, job: consts.ANALYZE, text }) + '\n');
+
+    this.messageId++;
+  }
+}
+
+module.exports = Evaluator;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const evaluator = require('./evaluator.js');
+const Evaluator = require('./evaluator.js');
 const trainer = require('./trainer.js');
 
 module.exports = {
-  evaluator,
+  Evaluator,
   trainer
 };
